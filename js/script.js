00ecd515538b11caaf020753f2c04deee2b629eb
jonnebonde <jonnebonde@gmail.com>
2022-02-03 15:36:33 +0100

    enter

--- js/script.js
+++ js/script.js
@@ -19,3 +19,45 @@ button.onclick = function () {
     }, 1000);
    
 };
+
+
+// string length and trim
+
+
+/* const animals = ["dog", "cat", "monkey"];
+
+console.log(animals.length)
+
+for(let i = 0; i < animals.length; i++) {
+    
+} */
+
+
+const name = " Blob ";
+
+const trimmedName = name.trim();
+
+console.log(name.length); // With spaces
+console.log(trimmedName.length); // without spaces on start and end of a string.
+
+
+
+// addEventListener suits well for multiple functions
+
+
+const myButton = document.querySelector(".btn");
+
+function functionA() {
+    console.log("AAAA");
+}
+
+function functionB() {
+    console.log("BBBB");
+}
+
+
+/* myButton.onclick = functionA; */
+
+myButton.addEventListener("click", functionA);
+myButton.addEventListener("click", functionB);
+
